a98ceb7e2f644b38bc6719690548443947212244
Web Ecology Project <code@webecologyproject.org>
2010-07-30 19:14:18 -0400

    Work on testing

--- back-end/cluster-code/test.rb
+++ back-end/cluster-code/test.rb
@@ -1,3 +1,31 @@
 load "run.rb"
-$w = Worker.new("worker-3")
-time_based_summary(975, "/raw_data/word_frequencies/")
+require 'fastercsv'
+ids = []
+FasterCSV.foreach("../../files/oldspice.csv") do |row|
+ids<< row[3]
+end
+
+scrape = Scrape.find({:id => 562})
+metadata = scrape.metadatas.first
+ids.each do |id|
+  debugger
+  url = "http://api.twitter.com/1/statuses/show/#{id}.json"
+  data = U.return_data(url)
+  if !data.nil?
+    data = JSON.parse(data)
+    if !data.empty?
+      user = UserHelper.hash_user(data["user"])
+      tweet = TweetHelper.hash_tweets([data])
+    end
+  end
+  users << user
+  tweets << tweet
+  if users.length > MAX_ROW_COUNT_PER_BATCH 
+    U.append_scrape_id({:users => users, :tweets => tweets}, metadata)
+    Database.save_all(:tweets => tweets)
+    Database.save_all(:users => users)
+  end
+end
+  
+# $w = Worker.new("worker-3")
+# time_based_summary(975, "/raw_data/word_frequencies/")
