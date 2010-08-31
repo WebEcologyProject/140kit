ba1dcfce906f66c14dc15c54f3f131d3e21b2738
Web Ecology Project <code@webecologyproject.org>
2010-08-30 21:07:34 -0700

    Disallow usernames with non alphanumeric characters

--- front-end/app/models/researcher.rb
+++ front-end/app/models/researcher.rb
@@ -20,7 +20,6 @@ class Researcher < ActiveRecord::Base
   before_save :encrypt_password
   before_save :set_times
   before_create :set_join_date
-  before_create :validate_characters
   
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
   def self.authenticate(user_name, password)
@@ -69,7 +68,7 @@ class Researcher < ActiveRecord::Base
     self.join_date = Time.now
   end
   
-  def validate_characters
+  def validate_on_create
     if !self.user_name.scan(/[\.\;\:\?\<\>\,\+\=\_\-\{\}\[\]\(\)\|\\\*\&\^%\$#\@\!\~\`]/).empty?
       errors.add("User Name", "You can only use alpha numeric characters in your user name (a-z, 0-9).")
     end
