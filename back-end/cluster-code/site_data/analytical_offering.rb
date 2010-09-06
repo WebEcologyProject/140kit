0e3585eb44b2ece10148cf6ddcdcd575a2b851f4
devin <dgaffney@eggs.(none)>
2010-09-06 16:04:31 -0400

    add in extra attrs for back-end

--- back-end/cluster-code/site_data/analytical_offering.rb
+++ back-end/cluster-code/site_data/analytical_offering.rb
@@ -1,3 +1,3 @@
 class AnalyticalOffering < SiteData
-  attr_accessor :id, :title, :description, :function, :source_code_link, :created_by, :created_by_link, :standard, :rest, :enabled, :save_path
+  attr_accessor :id, :title, :description, :function, :source_code_link, :created_by, :created_by_link, :standard, :rest, :enabled, :save_path, :access_level
 end
--- back-end/cluster-code/site_data/researcher.rb
+++ back-end/cluster-code/site_data/researcher.rb
@@ -1,5 +1,5 @@
 class Researcher < SiteData
-  attr_accessor :user_name, :email, :created_at, :updated_at, :suspended, :share_email
+  attr_accessor :user_name, :email, :created_at, :updated_at, :suspended, :share_email, :private_data, :hidden_account
   attr_accessor :remember_token, :remember_token_expires_at, :password_reset_code, :id, :salt, :crypted_password
   attr_accessor :last_login, :join_date, :last_access, :info, :website_url, :location, :pass_hash, :role, :reset_code
   attr_accessor :scrapes, :collections
